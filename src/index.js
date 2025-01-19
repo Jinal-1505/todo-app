86e013814fef73b0e5bc1fa38dbf64fad663fa25
Jinal-1505 <jinalmistry1505@gmail.com>
2025-01-19 21:59:50 +0530

    feat(setup): initialize express project and connect to db

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import indexRouter from './routes/index.route.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+const mongo_url = process.env.MONGO_URL;
+const port = process.env.PORT || 3000;
+
+app.use('/api', indexRouter);
+
+mongoose
+    .connect(mongo_url)
+    .then(() => console.log('Database Connected Successfully'))
+    .catch((error) => console.log('Database Connection failed', error));
+
+app.listen(port, () => console.log(`App is running on port ${port}`));
